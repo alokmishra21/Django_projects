b8b1b8cc5613e648a226403d99403b1014605255
H299664 <alok.mishra@honeywell.com>
2020-03-14 20:17:11 +0530

    added tabular inline view

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -9,18 +9,33 @@ admin.site.register(Genre)
 admin.site.register(Language)
 #admin.site.register(BookInstance)
 
-# Define the admin class
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
-    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
-# Register the admin class with the associated model
-admin.site.register(Author, AuthorAdmin)
+# Show the book instances in the book 
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    # extra will supress the extra empty rows
+    extra = 0
 
+	
 # Register the Admin classes for Book using the decorator
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+
+class BooksInline(admin.TabularInline):
+    model = Book
+    extra = 0
+	
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
 
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance) 
